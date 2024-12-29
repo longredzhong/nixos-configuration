d31880be3ae11dbabba9d4bf0a15179a01ae3312
longred <email@longred.work>
2024-12-29 21:37:46 +0800

    update k3s configuration to use specific proxy settings and add containerd proxy variables

--- nuc/k3s.nix
+++ nuc/k3s.nix
@@ -7,9 +7,12 @@
 }:
 let
   k3s_envfile = pkgs.writeText "k3s.env" ''
-    HTTP_PROXY=http://127.0.0.1:7890
-    HTTPS_PROXY=http://127.0.0.1:7890
+    HTTP_PROXY=http://nuc:7890
+    HTTPS_PROXY=http://nuc:7890
     NO_PROXY=127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
+    CONTAINERD_HTTP_PROXY=http://nuc:7890
+    CONTAINERD_HTTPS_PROXY=http://nuc:7890
+    CONTAINERD_NO_PROXY=127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
     '';
 in
 {
