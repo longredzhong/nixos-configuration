38b99fbf6e524906278079da9e80087fc4698dd7
longred <email@longred.work>
2024-12-29 18:24:42 +0800

    refactor mihomo.nix to use a variable for configFile fetching

--- nuc/mihomo.nix
+++ nuc/mihomo.nix
@@ -1,12 +1,15 @@
 { services
 , pkgs
 , ...
-}: {
+}: 
+let
+  mihomo_config = pkgs.fetchurl {
+    url = "https://proxy.longred.work/proxy/https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml";
+  };
+in {
   services.mihomo = {
     enable = true;
-    configFile = fetchurl {
-      url = "https://proxy.longred.work/proxy/https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml";
-    };
+    configFile = mihomo_config;
     webui = pkgs.metacubexd;
   };
 }
