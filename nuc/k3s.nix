f27d80eaf356b830daab96497689e309d2baa561
longred <email@longred.work>
2024-12-29 17:17:34 +0800

    add allowed ports for k3s networking and enable open-iscsi service

--- nuc/k3s.nix
+++ nuc/k3s.nix
@@ -1,11 +1,19 @@
 { networking
 , services
+, environment
 , ...
 }: {
   networking.firewall = {
     allowedTCPPorts = [
       6443
+      2379
       2380
+      10250
+    ];
+    allowedUDPPorts = [
+      8472
+      51820
+      51821
     ];
   };
 
@@ -15,8 +23,12 @@
     extraFlags = toString [
       "--advertise-address=100.127.172.105"
       "--node-ip=100.127.172.105"
-      "--embedded-registry"
     ];
+  };
+  environment.systemPackages = [ pkgs.nfs-utils ];
+  services.openiscsi = {
+    enable = true;
+    name = "${config.networking.hostName}-initiatorhost";
   };
 
 }
