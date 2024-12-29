d226cbe864a7f13fb98acada57c920fafa2d2dbd
longred <email@longred.work>
2024-12-29 18:22:30 +0800

    fix missing semicolon in mihomo.nix configuration

--- nuc/mihomo.nix
+++ nuc/mihomo.nix
@@ -8,5 +8,5 @@
       url = "https://proxy.longred.work/proxy/https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml";
     };
     webui = pkgs.metacubexd;
-  }
+  };
 }
