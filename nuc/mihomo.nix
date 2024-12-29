b94da2b0df7cb9554afc9c68576b2894728c26a7
longred <email@longred.work>
2024-12-29 18:25:44 +0800

    add sha256 checksum for mihomo configuration fetch

--- nuc/mihomo.nix
+++ nuc/mihomo.nix
@@ -5,6 +5,7 @@
 let
   mihomo_config = pkgs.fetchurl {
     url = "https://proxy.longred.work/proxy/https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml";
+    sha256 = "sha256-ubiNlcABGthkf9RJols0yuot4tuMLAFR9mUkJCpjTgM=";
   };
 in {
   services.mihomo = {
