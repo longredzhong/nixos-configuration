8d76ae6ba01180efad7579cc3572eadf56453d64
longred <email@longred.work>
2024-12-29 18:21:58 +0800

    fix syntax error in mihomo.nix configuration

--- nuc/mihomo.nix
+++ nuc/mihomo.nix
@@ -7,6 +7,6 @@
     configFile = fetchurl {
       url = "https://proxy.longred.work/proxy/https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml";
     };
-    webui = pkgs.metacubexd
-      }
-      }
+    webui = pkgs.metacubexd;
+  }
+}
