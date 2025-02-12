a1e97761bcf54bf1cf0d6fbd933cc229eb2ed3e4
longred <longredzhong@outlookl.com>
2025-02-12 22:53:11 +0800

    fix: add no_proxy environment variable to home.nix

--- wsl/home.nix
+++ wsl/home.nix
@@ -246,9 +246,12 @@ in
           set -e http_proxy
           set -e https_proxy
           set -e all_proxy
+          set -e no_proxy
           set -e HTTP_PROXY
           set -e HTTPS_PROXY
           set -e ALL_PROXY
+          set -e NO_PROXY
+
         '';
       };
       shellAbbrs =
