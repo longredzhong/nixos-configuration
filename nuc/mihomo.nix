d9f78a1588eea28f0a0b187bf84e45a47678339b
longred <longredzhong@outlookl.com>
2025-03-30 21:49:40 +0800

    fix: update URL in mihomo.nix to remove unnecessary proxy

--- nuc/mihomo.nix
+++ nuc/mihomo.nix
@@ -4,7 +4,7 @@
 }:
 let
   mihomo_config_file = pkgs.runCommand "mihomo-config.yaml" {} ''
-    ${pkgs.curl}/bin/curl -L "https://proxy.longred.work/proxy/https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml" -o $out
+    ${pkgs.curl}/bin/curl -L "https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml" -o $out
   '';
 in
 {
