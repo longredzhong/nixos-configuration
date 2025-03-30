d5f17d2d3e411f5cd23c8333c21224eb5bcf4962
longred <longredzhong@outlookl.com>
2025-03-30 21:55:03 +0800

    fix: update mihomo.nix to use fetchurl for configuration file retrieval

--- nuc/mihomo.nix
+++ nuc/mihomo.nix
@@ -3,9 +3,12 @@
 , ...
 }:
 let
-  mihomo_config_file = pkgs.runCommand "mihomo-config.yaml" {} ''
-    ${pkgs.curl}/bin/curl -L "https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/mihomo-party.yaml" -o $out
-  '';
+  mihomo_config_file = pkgs.fetchurl {
+    url = "https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/7524eeae2d0989e8f8816e24b8855009f538349c/mihomo-party.yaml";
+    # You'll need to add a sha256 hash. You can get it with:
+    # nix-prefetch-url https://gist.githubusercontent.com/longredzhong/1a8655edb00bad16cfb6890904fef602/raw/7524eeae2d0989e8f8816e24b8855009f538349c/mihomo-party.yaml
+    sha256 = "0nabmkzbv3m6dvrvp94iq4zj2lw69cwz9x3yx7whvyr5jzmnka1p"; # Replace with actual hash
+  };
 in
 {
   services.mihomo = {
