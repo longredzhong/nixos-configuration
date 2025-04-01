ff9d644e4bd6b927a39d44a7fcb731972fe7d4eb
longred <longredzhong@outlook.com>
2025-04-01 14:25:17 +0800

    fix: 更新 WSL 默认配置以修复 PostgreSQL 端口设置

--- nixos-config/hosts/wsl/default.nix
+++ nixos-config/hosts/wsl/default.nix
@@ -14,7 +14,7 @@
     enable = true;
     package = pkgs.postgresql_17;
     enableTCPIP = true;
-    port = 5432;
+    settings.port = 5432;
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
