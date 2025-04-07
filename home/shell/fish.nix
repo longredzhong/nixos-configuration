d37602856a46faa3bdf6857ebac244b9be51b399
longred <longredzhong@outlook.com>
2025-04-07 10:42:57 +0000

    优化 fish 和 starship 配置，提升历史记录限制和目录显示设置，更新 kubernetes 支持，添加字符和命令持续时间格式设置

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -1,46 +1,50 @@
-{ pkgs, unstable, ... }:
-{
+{ pkgs, unstable, ... }: {
   programs.fish = {
     enable = true;
+    package = unstable.fish;
+
     interactiveShellInit = ''
       set -U fish_greeting
-      # 提高历史记录限制
+      # Increase history limit
       set -g fish_history_max_length 10000
     '';
+
     functions = {
       refresh = "source $HOME/.config/fish/config.fish";
       take = ''mkdir -p -- "$1" && cd -- "$1"'';
-      ttake = "cd $(mktemp -d)";
-      show_path = "echo $PATH | tr ' ' '\n'";
+      ttake = "cd (mktemp -d)"; # More idiomatic fish syntax
+      show_path = "string split ' ' $PATH"; # More efficient way
       posix-source = ''
         for i in (cat $argv)
-          set arr (echo $i |tr = \n)
+          set arr (string split = $i)
           set -gx $arr[1] $arr[2]
         end
       '';
     };
-    shellAbbrs =
-      {
-        ".." = "cd ..";
-        "..." = "cd ../../";
-        "...." = "cd ../../../";
-        "....." = "cd ../../../../";
-      }
-      // {
-        gapa = "git add --patch";
-        grpa = "git reset --patch";
-        gst = "git status";
-        gdh = "git diff HEAD";
-        gp = "git push";
-        gph = "git push -u origin HEAD";
-        gco = "git checkout";
-        gcob = "git checkout -b";
-        gcm = "git checkout master";
-        gcd = "git checkout develop";
-        gsp = "git stash push -m";
-        gsa = "git stash apply stash^{/";
-        gsl = "git stash list";
-      };
+
+    shellAbbrs = {
+      # Directory navigation
+      ".." = "cd ..";
+      "..." = "cd ../../";
+      "...." = "cd ../../../";
+      "....." = "cd ../../../../";
+
+      # Git abbreviations
+      gapa = "git add --patch";
+      grpa = "git reset --patch";
+      gst = "git status";
+      gdh = "git diff HEAD";
+      gp = "git push";
+      gph = "git push -u origin HEAD";
+      gco = "git checkout";
+      gcob = "git checkout -b";
+      gcm = "git checkout master";
+      gcd = "git checkout develop";
+      gsp = "git stash push -m";
+      gsa = "git stash apply stash^{/";
+      gsl = "git stash list";
+    };
+
     plugins = [
       {
         inherit (pkgs.fishPlugins.autopair) src;
@@ -51,8 +55,9 @@
         name = "sponge";
       }
     ];
+
     shellInit = ''
-      # 设置PATH
+      # Set PATH
       fish_add_path $HOME/.local/bin
       fish_add_path $HOME/.cargo/bin
     '';
--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -1,19 +1,42 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   programs.starship = {
     enable = true;
     settings = {
       aws.disabled = true;
       gcloud.disabled = true;
-      kubernetes.disabled = true;
+      kubernetes.disabled = false;
       git_branch.style = "242";
       directory.style = "blue";
-      directory.truncate_to_repo = false;
+      directory.truncate_to_repo = true;
       directory.truncation_length = 8;
       python.disabled = false;
       ruby.disabled = true;
       hostname.ssh_only = false;
       hostname.style = "bold green";
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[✗](bold red)";
+      };
+      cmd_duration = {
+        min_time = 500;
+        format = "took [$duration](bold yellow)";
+      };
+      nix_shell = {
+        symbol = " ";
+        format = "via [$symbol$state]($style) ";
+      };
+      format = "$all";
+      add_newline = true;
+      line_break.disabled = false;
+      package.disabled = false;
+      rust.format = "via [🦀 $version](red bold)";
+      nodejs.format = "via [⬢ $version](green bold)";
+      time = {
+        disabled = false;
+        format = "[$time]($style) ";
+        time_format = "%R";
+        style = "bright-black";
+      };
     };
   };
 }
