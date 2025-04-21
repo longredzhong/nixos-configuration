6daaf2d65b185b176b9cbf900cf979fcf0db6821
longred <longredzhong@outlook.com>
2025-04-21 06:50:29 +0000

    feat(thinkingbook-wsl): ✨ 添加 X11 相关服务配置
    * 启用 X11 服务
    * 配置 SDDM 显示管理器
    * 设置 Plasma 6 桌面管理器
    * 默认会话设置为 "plasmax11"

--- hosts/thinkbook-wsl/default.nix
+++ hosts/thinkbook-wsl/default.nix
@@ -42,4 +42,11 @@
     useWindowsDriver = true;
     populateBin = true;
   };
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmax11";
+
 }
